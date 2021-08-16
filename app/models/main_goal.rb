0c0fad7b36a1e7dcabfd2794d88af9e8e79bf14c
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-17 08:54:59 +0900

    バリテーション設定

--- app/models/main_goal.rb
+++ app/models/main_goal.rb
@@ -1,3 +1,7 @@
 class MainGoal < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :main_goal
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_namy :main_goals
+  validates :name, presence: true
 end
