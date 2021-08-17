7bf0c4762424bcf9127f584f7833f729895f2d91
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-17 14:51:48 +0900

    バリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :main_goals
+  has_many :sub_goals
   validates :name, presence: true
 end
