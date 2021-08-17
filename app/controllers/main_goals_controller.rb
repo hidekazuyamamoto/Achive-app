ecc4c2fd91696367225c6f0523dae70e0d528547
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-17 14:09:00 +0900

    編集機能実装

--- app/controllers/main_goals_controller.rb
+++ app/controllers/main_goals_controller.rb
@@ -19,6 +19,13 @@ class MainGoalsController < ApplicationController
   def edit
     @main_goal = MainGoal.find(params[:id])
   end
+  def update
+    if MainGoal.update(main_goal_params)
+      redirect_to  main_goal_path(current_user.id)
+    else
+      render :edit
+    end
+  end
   private
   def main_goal_params
     params.require(:main_goal).permit(:main_goal, :explnation, :dead_line).merge(user_id: current_user.id)
