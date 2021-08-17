a0e1765e58186d90635cfc44f5dca308be541b83
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-17 14:01:47 +0900

    editコントローラー実装

--- app/controllers/main_goals_controller.rb
+++ app/controllers/main_goals_controller.rb
@@ -16,6 +16,9 @@ class MainGoalsController < ApplicationController
   def show
     @main_goal = MainGoal.where(user_id: params[:id])
   end
+  def edit
+    @main_goal = MainGoal.find(params[:id])
+  end
   private
   def main_goal_params
     params.require(:main_goal).permit(:main_goal, :explnation, :dead_line).merge(user_id: current_user.id)
