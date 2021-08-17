2539007e797b49c5212c526cccee889f59794939
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-18 08:57:05 +0900

    カラム追加

--- db/migrate/20210817231822_create_profiles.rb
+++ db/migrate/20210817231822_create_profiles.rb
@@ -5,6 +5,8 @@ class CreateProfiles < ActiveRecord::Migration[6.0]
       t.timestamps
       t.references :user, nill: false, foreign_key: true
       t.text :profile
+      t.string :life_goal
+      t.text :life_goal_explanation
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2021_08_17_232028) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.text "profile"
+    t.string "life_goal"
+    t.text "life_goal_explanation"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
