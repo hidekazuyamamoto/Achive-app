58421984344b41675e20f6002084880acd606df8
hidekazuyamamoto <87454058+hidekazuyamamoto@users.noreply.github.com>
2021-08-18 12:26:50 +0900

    データ保存完了

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,9 +3,19 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   def create
+    @profile = Profile.new(params_profile)
+    if @profile.save
+      redirect_to  main_goal_path(current_user.id)
+    else
+      render :new
+    end 
   end
   def edit
   end
   def update
+  end
+  private
+  def params_profile
+    params.require(:profile).permit(:name, :image, :life_goal, :life_goal_explanation, :profile).merge(user_id: current_user.id)
   end
 end
